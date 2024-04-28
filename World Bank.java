c9175fba1f1d0c49066195c69851c7d944ef5b1c
pgchiranjeevi <chiranjeevipg211@gmail.com>
2024-04-28 11:24:06 +0530

    world first

---
+++ World Bank.java
@@ -0,0 +1,19 @@
+package com.poornima;
+
+public class bank {
+
+	public static void atm() {
+		
+		System.out.println("hi, welcome to the world banking");
+                 
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		atm();
+		
+	}
+	
+}
