87a648c0758b8aa8cbdbf3ba62eef5a00cfd2084
pgchiranjeevi <chiranjeevipg211@gmail.com>
2024-04-28 11:07:08 +0530

    number added

---
+++ bank.java
@@ -0,0 +1,20 @@
+package com.poornima;
+
+public class bank {
+
+	public static void atm() {
+		
+		System.out.println("hi, welcome to the banking");
+                 12345
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		atm();
+		
+	}
+	
+}
